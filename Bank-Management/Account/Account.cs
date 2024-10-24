663a76891648c39eda227352e9db67fcdb09f6df
Lim Cuan Hei <cuanhei0171@gmail.com>
2024-10-24 17:59:49 +0800

    Update Person & Account

---
+++ Bank-Management/Account/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    public class Account
+    {
+        private int id;
+        private string username;
+        private string password;
+        private BankAccount[] bankAccounts;
+
+        private static int numOfAcc;
+        private static readonly int MAX_BANK_ACCOUNT = 10;
+
+        public Account(){
+
+            this.id = ++numOfAcc;
+            this.username = "-";
+            this.password = "-";
+            this.bankAccounts = new BankAccount[MAX_BANK_ACCOUNT];
+        }
+        public Account(String username, String password): this() {
+
+            Username = username;
+            Password = password;
+        }
+
+        public int Id { get { return this.id; } }
+
+        public String Username {
+            get { return this.username; }
+            set { 
+                if(true)
+                    username = value;
+            }
+        }
+
+        public String Password
+        {
+            get { return this.password; }
+            set
+            {
+                if (true)
+                    password = value;
+            }
+        }
+
+        public void cancelCreate() {
+            numOfAcc--;
+        }
+    }
+}
---
+++ Bank-Management/Account/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    public class Person
+    {
+        private String name;
+        private int age;
+        private String ic;
+        private String email;
+
+        public Person()
+        {
+        }
+
+        public Person(string name, int age, string ic)
+        {
+            this.name = name;
+            this.age = age;
+            this.ic = ic;
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length <= 20 && value.Length >= 1)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine("[Name length must between 1 to 20]");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 18 && value <= 100)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("[You can only register when you are above 18 Years Old & below 100 Years Old]");
+                }
+            }
+        }
+
+        public String Ic
+        {
+            get { return ic; }
+            set
+            {
+                if (value.Length == 12)
+                {
+                    ic = value;
+                }
+                else
+                {
+                    Console.WriteLine("[IC must contains 12 digits]");
+                }
+            }
+        }
+
+        public String toString()
+        {
+            return "Name: " + name + "\n" +
+                   "Age: " + age + "\n" +
+                   "IC: " + ic + "\n";
+        }
+    }
+
+}
